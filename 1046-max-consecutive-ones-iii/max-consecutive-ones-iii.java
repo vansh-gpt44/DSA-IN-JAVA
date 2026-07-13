83205c1b585274d03404c248a0613d3118459ff6
vansh_gpt44 <156524605+vansh-gpt44@users.noreply.github.com>
2026-07-14 01:35:30 +0530

    Time: 3 ms (96.22%) | Memory: 52 MB (73.93%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,22 +1,43 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int maxlen=0;
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            int zeros=0;
-            for(int j=i;j<n;j++){
-                if(nums[j]==0){
-                    zeros++;
-                }
-                if(zeros<=k){
-                    int len=j-i+1;
-                    maxlen=Math.max(len,maxlen);
-                }
-                else{
-                    break;
-                }
+        int maxlen=0,l=0,r=0,n=nums.length,zero=0;
+        while(r<n){
+            if(nums[r]==0){ 
+            zero++;
             }
+            if(zero>k){
+                if(nums[l]==0){
+                    zero--;   
+                }
+                l++;
+            }
+            if(zero<=k){
+                int len=r-l+1;
+                maxlen=Math.max(maxlen,len);
+            }
+            r++;
         }
         return maxlen;
+
+
+//=======================Brute Force========================================
+        // int maxlen=0;
+        // int n=nums.length;
+        // for(int i=0;i<n;i++){
+        //     int zeros=0;
+        //     for(int j=i;j<n;j++){
+        //         if(nums[j]==0){
+        //             zeros++;
+        //         }
+        //         if(zeros<=k){
+        //             int len=j-i+1;
+        //             maxlen=Math.max(len,maxlen);
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        // }
+        // return maxlen;
     }
 }
